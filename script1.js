999876b6de3912b429e6e9a0c923fd361cd601b4
Rustam-Ulaevich <praporglamur@gmail.com>
2024-10-31 15:17:31 +0500

    first commit

---
+++ script1.js
@@ -0,0 +1,20 @@
+// 1 task
+// let nameUser = prompt('Введите своё имя:')
+// alert(`Привет, ${nameUser}`)
+
+//2 task
+// const year = 2024
+// let yearBirth = prompt('Вы какого года рождения ?')
+// let age = year - yearBirth
+// alert(`Вам ${age} лет`)
+
+//3 task
+// let length = prompt('Введите длину квадрата:')
+// alert(`Периметр квадрата составлет ${length*4}м`)
+
+//4task
+// let radius = prompt('Введите радиус окружности:')
+// alert(`Площадь окружности составлет ${3.14*radius*radius}м2`)
+// alert(`Площадь окружности составлет ${3.14*(radius**2)}м2`)
+// alert(`Площадь окружности составлет ${3.14*Math.pow(radius)}м2)
+
